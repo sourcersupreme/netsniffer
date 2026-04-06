11fe57cced31841dcffddc0d79dc647cd8e6e441
SoulReaper <81975481+sourcersupreme@users.noreply.github.com>
2026-04-06 20:58:53 +0530

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -2,13 +2,13 @@ from scapy.all import sniff, IP, TCP, Raw
 
 def process_packet(packet):
     if packet.haslayer(IP):
-        print(f"\n[+] Packet: {packet[IP].src} -> {packet[IP].dst}")
-
-    if packet.haslayer(TCP):
-        print(f"[+] TCP Port: {packet[TCP].sport} -> {packet[TCP].dport}")
+        print(f"\n[+] {packet[IP].src} -> {packet[IP].dst}")
 
     if packet.haslayer(Raw):
-        print(f"[+] Data: {packet[Raw].load}")
+        try:
+            data = packet[Raw].load.decode()
+            print("[+] Data:", data)
+        except:
+            pass
 
-print("Sniffer Started...\n")
 sniff(prn=process_packet, store=False)
