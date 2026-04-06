dd2619b0027e9a20d6f9a7d24e21ab484a7712ce
SoulReaper <81975481+sourcersupreme@users.noreply.github.com>
2026-04-06 21:08:17 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+INTERFACE = "eth0"
+
+# Detection settings
+PORT_SCAN_THRESHOLD = 10
+TIME_WINDOW = 10
+
+# Email settings
+EMAIL_SENDER = "your_email@gmail.com"
+EMAIL_PASSWORD = "your_app_password"
+EMAIL_RECEIVER = "your_email@gmail.com"
+
+# GeoIP DB path
+GEOIP_DB = "GeoLite2-City.mmdb"
